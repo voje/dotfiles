47c5168e317ae07ac9df6b74ed70d6014fcefd17
voje <kristjan.voje@gmail.com>
2023-05-03 17:47:31 +0200

    adsf

--- .config/nvim/lua/prime/packer.lua
+++ .config/nvim/lua/prime/packer.lua
@@ -5,17 +5,22 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.1',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
-	use {
-		'rose-pine/neovim',
-		as = 'rose-pine',
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
+        'rose-pine/neovim',
+        as = 'rose-pine',
 		config = function()
 			require("rose-pine").setup()
 			vim.cmd('colorscheme rose-pine')
