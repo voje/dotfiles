f2c4c0a0d7aa5611399ae4d65c78ea3517a26c7c
voje <voje@iskratel.si>
2024-02-01 19:21:04 +0100

    Neorg config update

--- .config/nvim/lua/custom/lazy.lua
+++ .config/nvim/lua/custom/lazy.lua
@@ -189,29 +189,16 @@ require('lazy').setup({
 
   -- Neorg
   {
-	'nvim-orgmode/orgmode',
-	dependencies = {
-	  { 'nvim-treesitter/nvim-treesitter', lazy = true },
-	},
-	event = 'VeryLazy',
-	config = function()
-	  -- Load treesitter grammar for org
-	  require('orgmode').setup_ts_grammar()
-
-	  -- Setup treesitter
-	  require('nvim-treesitter.configs').setup({
-		highlight = {
-		  enable = true,
-		  additional_vim_regex_highlighting = { 'org' },
-		},
-		ensure_installed = { 'org' },
-	  })
-
-	  -- Setup orgmode
-	  require('orgmode').setup({
-		org_agenda_files = '~/orgfiles/**/*',
-		org_default_notes_file = '~/orgfiles/refile.org',
-	  })
-	end,
+    "nvim-neorg/neorg",
+    -- run = ":Neorg sync-parsers", -- This is the important bit!
+    -- config = function()
+    config = function()
+      require("neorg").setup {
+        load = {
+          ['core.defaults'] = {},
+          ['core.export'] = {},
+        },
+      }
+    end
   }
 }, {})
